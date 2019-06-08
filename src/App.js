8bbac8e8ffe63cc2259b4eaf180cab1dd59aad1a
hiroki <beats.1992@gmail.com>
2019-06-08 21:39:12 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        hello world
+        <Test c="blue" />
+        <Test c="skyblue" />
+       </div>
+    );
+  }
+}
+function Test(props){
+  return(
+    <div style={{color:props.c}}>testteesrst</div>
+  )
+}
+
+export default App;
